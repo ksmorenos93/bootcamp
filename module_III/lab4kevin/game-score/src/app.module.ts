8b34fc96575f0a4c9fc16093359f6e2bbf752047
ksmorenos93 <ksmorenos@unal.edu.co>
2024-11-14 17:42:05 -0500

    lab4 kevin

--- module_III/lab4kevin/game-score/src/app.module.ts
+++ module_III/lab4kevin/game-score/src/app.module.ts
@@ -1,13 +1,23 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './modules/users/users.service';
-import { ScoresService } from './modules/scores/scores.service';
-import { AuthService } from './modules/auth/auth.service';
+import { UsersModule } from './modules/users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ScoresModule } from './modules/scores/scores.module';
+import { AuthModule } from './modules/auth/auth.module';
+//import { PrismaModule } from '../prisma/prisma.module'; // Corrected import path
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule,
+    UsersModule,    // Register feature modules
+    ScoresModule
+  ],
   controllers: [AppController],
-  providers: [AppService, UsersService, ScoresService, AuthService],
+  providers: [AppService],
 })
 export class AppModule {}
--- module_III/lab4kevin/game-score/src/main.ts
+++ module_III/lab4kevin/game-score/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger' ;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuracion basica de Swagger
+  const config = new DocumentBuilder ()
+  .setTitle('Game Score Api')
+  .setDescription('REST API for Game Score API')
+  .setVersion('1.4.15')
+  .addServer('/api/v1')
+  .addBearerAuth()
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  app.enableVersioning().setGlobalPrefix('api/v1')
+  
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
